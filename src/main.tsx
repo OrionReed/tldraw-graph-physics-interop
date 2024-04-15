290acc2daefe6148e163df90c23f06bbea694f30
Orion Reed <orionreedt@gmail.com>
2024-04-15 15:10:18 +0100

    lil commit so gh pages deploys

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,7 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./css/index.css";
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
+ReactDOM.createRoot(document.getElementById("root") ?? document.body).render(
 	<App />
 );
